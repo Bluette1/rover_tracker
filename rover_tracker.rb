6f700b6da10bbd87762b6d424fca346b72a8e838
Bluette1 <marylene.sawyer@gmail.com>
2021-07-22 02:15:54 +0200

    Fix lint errors

--- rover_tracker.rb
+++ rover_tracker.rb
@@ -1,107 +1,97 @@
-def heading_map (ch)
-  heading = nil
-  
-  if (ch == 'N')
-    heading = [0, 1]   
-  elsif (ch == 'S')
-    heading = [0, -1] 
-  elsif (ch == 'E')
-    heading = [1, 0]   
+def heading_map(char)
+  case char
+  when 'N'
+    [0, 1]
+  when 'S'
+    [0, -1]
+  when 'E'
+    [1, 0]
   else
-    heading = [-1, 0]
+    [-1, 0]
   end
-  heading
 end
 
-def ch_map (heading)
-  ch = nil
-  
-  if (heading == [0, 1])
-    ch =  'N'  
-  elsif (heading ==[0, -1])
-    ch = 'S'
-  elsif (heading == [1, 0])
-    ch = 'E'
+def ch_map(heading)
+  case heading
+  when [0, 1]
+    'N'
+  when [0, -1]
+    'S'
+  when [1, 0]
+    'E'
   else
-    ch = 'W'
+    'W'
   end
-  ch
 end
 
 def angle_deg(size)
   size += 90
-  if (size >= 180)
-    size = 90
-  end
+  size = 90 if size >= 180
   size
 end
 
-def turn (posn, turn, angle)
+def turn(posn, turn, angle)
   x_posn = posn[0]
   y_posn = posn[1]
-  res = nil
-  if (angle == 90)
-    if (turn == 'L')
-      res = [-y_posn, x_posn]
+  if angle == 90
+    if turn == 'L'
+      [-y_posn, x_posn]
     else
-      res = [y_posn, -x_posn]
-    end 
+      [y_posn, -x_posn]
+    end
   else
-    res = [-x_posn, -y_posn] 
+    [-x_posn, -y_posn]
   end
-  res
 end
 
-def add(posn_1, posn_2)
-  x_posn = posn_1[0] + posn_2[0]
-  y_posn = posn_1[1] + posn_2[1]
+def add(posn_one, posn_two)
+  x_posn = posn_one[0] + posn_two[0]
+  y_posn = posn_one[1] + posn_two[1]
   [x_posn, y_posn]
 end
 
-def bounds_exceeded? (posn, bounds)
-  exceeded = false 
-  if (posn[0] > bounds[0].to_i || posn[1] > bounds[1].to_i || posn[0] < 0 || posn[1] < 0)
-    exceeded = true
-  end
+def bounds_exceeded?(posn, bounds)
+  exceeded = false
+  exceeded = true if posn[0] > bounds[0].to_i || posn[1] > bounds[1].to_i || (posn[0]).negative? || (posn[1]).negative?
   exceeded
 end
 
 def rover_tracker(input)
- bounds = input[0].split
+  bounds = input[0].split
 
- end_posns = []
+  end_posns = []
 
- (1..input.length - 1).each do |idx|
-  angle = 0
-  rover = input[idx].split
-  x_posn = rover[0].to_i
-  y_posn = rover[1].to_i
-  posn = [x_posn, y_posn]
-  heading = heading_map(rover[2])
-  moves = rover[3].split('')
+  (1..input.length - 1).each do |idx|
+    angle = 0
+    rover = input[idx].split
+    x_posn = rover[0].to_i
+    y_posn = rover[1].to_i
+    posn = [x_posn, y_posn]
+    heading = heading_map(rover[2])
+    moves = rover[3].split('')
 
-  for value in moves do 
+    moves.each do |value|
+      if bounds_exceeded?(posn, bounds)
+        p 'Bounds exceeded!'
+        return nil
+      end
 
-    if (bounds_exceeded?(posn, bounds))
-      p 'Bounds exceeded!'
-      return
+      if value == 'M'
+        posn = add(heading, posn)
+      else
+        angle = angle_deg(angle)
+        heading = turn(heading, value, angle)
+      end
     end
-
-    if(value == 'M')
-      posn = add(heading, posn)
-    else
-      angle = angle_deg(angle)
-      heading = turn(heading, value, angle)
-    end
+    posn << ch_map(heading)
+    end_posns << posn
   end
-  posn << ch_map(heading)
-  end_posns << posn
- end
- end_posns
+  end_posns
 end
 
 input_file = File.open('input.txt')
 input = input_file.readlines.map(&:chomp)
+input_file.close
 
 output = rover_tracker(input)
 
